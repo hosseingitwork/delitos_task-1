510a8a857a87f02f488099280c147f6bbb08f43c
Hossein <118553327+hosseinYah@users.noreply.github.com>
2023-05-12 01:12:05 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+
+### @autor Hossein Yahyazadeh
+### pcd systems challenge
+
+# 1. Read the file ‘delitos_fuero_comun.xlsx’
+
+# 2. In each cell, remove unneeded white spaces before and after the text
+
+# 3. Execute on Python the SQL query required to create a table called ‘delitos’.
+# (Drop the table if exists, so there is no previous data)
+# a. Use the same column names as in the Excel file.
+
+# 4. Create the SQL queries to insert the rows where ‘Delitos_fuero_comun’ is
+# higher than 100. Preferably using a transaction.
+
+# 5. Save all the queries into a file ‘queries_[DATE].txt’.
+# a. Replace [DATE] with the current date using the format ‘2023_05_04’
+# for May 4th, 2023.
+
+# 6. Execute the queries to the DB.
